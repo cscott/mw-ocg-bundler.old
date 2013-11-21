428dbba44250266c6e162a2e1b0de48d3e539b78
C. Scott Ananian <cscott@cscott.net>
2013-11-21 15:16:46 -0500

    Factor out promise utility module 'P', shared with mw-latexer.
    Change-Id: Ida24d906a37afa557b9b093f91e4ef038b37d6f1

--- test/samples.js
+++ test/samples.js
@@ -1,10 +1,10 @@
 /* global describe, it */
 var assert = require("assert");
 var fs = require('fs');
-var nodefn = require("when/node/function");
 var path = require('path');
 
 var bundler = require('../');
+var P = require('../lib/p');
 
 // ensure that we don't crash on any of our sample inputs
 describe("Basic crash test", function() {
@@ -13,7 +13,7 @@ describe("Basic crash test", function() {
 			it('should bundle', function(done) {
 				this.timeout(0);
 				var filename = path.join(__dirname, '..', 'samples', name);
-				return nodefn.call(fs.readFile.bind(fs), filename, 'utf8')
+				return P.call(fs.readFile, fs, filename, 'utf8')
 					.then(function(metabook) {
 						metabook = JSON.parse(metabook);
 						return bundler.bundle(metabook, {
