0baa0c5efd4d5fe080353301e057793ecf89a87e
C. Scott Ananian <cscott@cscott.net>
2013-11-22 15:49:09 -0500

    Improve script to build featured article collections.
    Change-Id: I2969662ecd0fa7de5a08c910efac8935803cdc02

--- samples/featured.js
+++ samples/featured.js
@@ -2,15 +2,33 @@
 require('es6-shim');
 /** Generate bundles from the featured articles list. */
 
+
+var program = require('commander');
+
 var fs = require('fs');
 var path = require('path');
+var rimraf = require('rimraf');
 var url = require('url');
 
 var P = require('../lib/p');
 
+program
+	.version(require('../').version)
+	.usage('[options]')
+	.option('-f, --featured <featuredarticles.txt>',
+			'Use specified list of featured articles',
+			path.join(__dirname, 'featuredarticles.txt'))
+	.option('-s, --skip',
+			'Skip collections which already exist on disk')
+	.option('-v, --verbose',
+			'Print verbose progress information')
+	.option('-D, --debug',
+			'Turn on debugging features (eg, full stack traces on exceptions)');
+program.parse(process.argv);
+
 var articles = Object.create(null);
 fs.
-	readFileSync(path.join(__dirname,'featuredarticles.txt'), 'utf8').
+	readFileSync(program.featured, 'utf8').
 	replace(/^\s+/, '').
 	replace(/\s+$/, '').
 	split(/[\r\n]+/).forEach(function(articleUrl) {
@@ -27,10 +45,18 @@ fs.
 // (cross-wiki stuff will have to wait)
 P.forEachSeq(Object.keys(articles), function(prefix) {
 	var outfile = prefix + '.zip';
-	var args = [ '-D', '-v', '-o', outfile, '-p', prefix ].
+	if (program.skip && fs.existsSync(path.join(__dirname, outfile))) {
+		if (program.verbose) { console.log('Skipping', outfile); }
+		return;
+	}
+	var args = [];
+	if (program.debug) { args.push('-D'); }
+	if (program.verbose) { args.push('-v'); }
+	args = args.concat([ '-o', outfile, '-p', prefix ]).
 		concat(articles[prefix]);
 	console.log('mw-bundler', args.join(' '));
-	return P.call(fs.unlink, fs, path.join(__dirname, outfile)).
+	return P.call(rimraf, null, path.join(__dirname, outfile)).
+		then(function() { }, function() { /* ignore unlink errors */ }).
 		then(function() {
 			return P.spawn(path.join(__dirname, '..', 'bin', 'mw-bundler'),
 						   args, { cwd: __dirname, stdio: 'inherit' });
